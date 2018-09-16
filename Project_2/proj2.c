b5d7e0e18d0ae6d7d15e15d784babe7b57466be8
Jae-Won Jang <jjang3@vt.edu>
2018-09-16 18:34:11 -0400

    finished proj2

--- Project_2/proj2.c
+++ Project_2/proj2.c
@@ -1,14 +1,16 @@
 #include <linux/init.h>
 #include <linux/kernel.h>
-#include <linux/module.h>
 #include <linux/slab.h>
 #include <linux/list.h>
 #include <linux/errno.h>
 #include <linux/module.h>
 #include <linux/seq_file.h>
 #include <linux/proc_fs.h>
-
 #include <linux/types.h>
+#include <linux/hashtable.h>
+#include <linux/rbtree.h>
+#include <linux/radix-tree.h>
+
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Jae-Won Jang");
@@ -20,10 +22,16 @@ module_param(int_str, charp, S_IRUSR | S_IRGRP | S_IROTH);
 MODULE_PARM_DESC(int_str, "A comma-separated list of integers");
 
 static LIST_HEAD(mylist);
+static DEFINE_HASHTABLE(myHTable, 3);
+struct rb_root root = RB_ROOT;
+RADIX_TREE(radix_tree, GFP_NOIO);
 
 struct entry {
   int val;
   struct list_head list;
+  struct hlist_node hash;
+  struct rb_node rbnode;
+  struct radix_tree_node radix_node;
 };
 
 static int store_value(int val)
@@ -32,7 +40,61 @@ static int store_value(int val)
   prj2_list = kmalloc(sizeof(*prj2_list), GFP_KERNEL);
   prj2_list->val = val;
   list_add_tail(&prj2_list->list, &mylist);
+  return 0;
 }
+
+static int store_hash_value(int val){
+
+  struct entry *myHash;
+  myHash = kmalloc(sizeof(*myHash), GFP_KERNEL);
+  myHash->val = val;
+  hash_add(myHTable, &myHash->hash, myHash->val);
+  return 0;
+}
+
+int insert_rb_tree(struct rb_root *tree_root, struct entry *insertion)
+{
+  struct rb_node **new = &(tree_root->rb_node);
+  struct rb_node *parent = NULL;
+
+  while (*new){
+    struct entry *this = container_of(*new, struct entry, rbnode);
+    parent = *new;
+    if (this->val > insertion->val)
+      {
+        new = &(*new)->rb_left;
+      }
+    else if (this->val < insertion->val)
+      {
+        new = &(*new)->rb_right;
+      }
+    else
+      {
+        return -1;
+      }
+  }
+  rb_link_node(&insertion->rbnode, parent, new);
+  rb_insert_color(&insertion->rbnode, tree_root);
+  return 0;
+}
+
+static int rb_tree_insert(int value)
+{                                  
+  struct entry *target = kmalloc(sizeof(*target), GFP_KERNEL);
+  target->val = value;
+  insert_rb_tree(&root, target);
+  return 0;
+}
+
+static int radix_insert(int key)
+{
+  struct entry *rnode;
+  rnode = kmalloc(sizeof(*rnode), GFP_KERNEL);
+  rnode-> val = key;
+  radix_tree_insert(&radix_tree, key, rnode);
+  return 0;
+}
+
 
 static int parse_params(void)
 {
@@ -51,6 +113,9 @@ static int parse_params(void)
     if (err)
       break;
     err = store_value(val);
+    err = store_hash_value(val);
+    err = rb_tree_insert(val);
+    err = radix_insert(val);
     if (err)
       break;
   }
@@ -62,12 +127,54 @@ static void test_linked_list(void)
 {
   struct list_head *position = NULL;
   struct entry *dataptr = NULL;
+  printk("Linked List = ");
   list_for_each(position, &mylist)
     {
       dataptr = list_entry(position, struct entry, list);
-      printk("data in the list = %d\n", dataptr->val);
+      printk(KERN_CONT "%d ", dataptr->val);
     }
 }
+
+static void test_hash_table(void){
+  int bucket;
+  struct entry *curpos;
+  printk("Hash Table = ");
+  hash_for_each(myHTable, bucket, curpos, hash)
+    {
+      printk(KERN_CONT "%d ", curpos->val);
+    }
+}
+
+static void rb_tree_view(struct rb_root *root)
+{
+  struct rb_node *iternode;
+  printk("R-B Tree = ");
+  for (iternode = rb_first(root); iternode; iternode = rb_next(iternode))
+    {
+      struct entry *temp = rb_entry(iternode, struct entry, rbnode);       
+      printk(KERN_CONT "%d ", temp->val);
+    }
+}
+
+static void view_radix(void)
+{
+  void **slot;
+  struct radix_tree_iter iter;
+  struct entry *temp = kmalloc(sizeof(*temp), GFP_KERNEL);
+  printk("Radix Tree = ");
+  radix_tree_for_each_slot(slot, &radix_tree, &iter, 0)
+   {
+     temp = radix_tree_lookup(&radix_tree, iter.index);
+     if(temp == NULL)
+       {
+         return;
+       }
+     else{
+       printk(KERN_CONT "%d ", temp->val);
+     }
+   }
+}
+
 
 static void destroy_linked_list_and_free(void)
 {
@@ -80,20 +187,56 @@ static void destroy_linked_list_and_free(void)
       list_del(pos);
       kfree(data);
     }
-  
+}
+static void remove_hash_table(void){
+  int bucket;
+  struct entry *pos;
+  struct hlist_node *tmp = NULL;
+  hash_for_each_safe(myHTable, bucket, tmp, pos, hash)
+    {
+      hash_del(&pos->hash);
+      kfree(pos);
+    }
+  kfree(tmp);
+}
+
+static void rb_tree_remove(struct rb_root *root)
+{
+  struct rb_node *iternode;
+  for (iternode = rb_first(root); iternode; iternode = rb_next(iternode))
+    {
+      struct entry *temp = rb_entry(iternode, struct entry, rbnode); 
+      rb_erase(&(temp->rbnode), root);
+      kfree(temp);
+    }
+}
+
+static void radix_remove(void)
+{
+  void **slot;
+  struct radix_tree_iter iter;
+  radix_tree_for_each_slot(slot, &radix_tree, &iter, 0)
+    {
+      radix_tree_delete(&radix_tree, iter.index);
+    }
 }
 
 static void run_tests(void)
 {
   test_linked_list();
+  test_hash_table();
+  rb_tree_view(&root);
+  view_radix();
   // add other tests here
-
 }
 
 static void cleanup(void)
 {
   printk(KERN_INFO "\nCleaning up...\n");
   destroy_linked_list_and_free();
+  remove_hash_table();
+  rb_tree_remove(&root);
+  radix_remove();
 }
 
 static int proj2_proc_show(struct seq_file *m, void *v)
@@ -103,7 +246,6 @@ static int proj2_proc_show(struct seq_file *m, void *v)
     printk(KERN_INFO "Missing \'int_str\' parameter, exiting \n");
     return -1;
   }
-  printk("Hello v2!\n");
   err = parse_params();
   if (err)
     goto out;
@@ -136,7 +278,6 @@ static void __exit prj2_exit(void)
 {
   remove_proc_entry("proj2", NULL);
 }
-
 
 module_init(prj2_init);
 module_exit(prj2_exit);
---
+++ Project_2/test-proj2.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+sudo insmod proj2.ko int_str=1,2,3,4,5
+cat /proc/proj2
+rmmod proj2
+dmesg
+dmesg -c > cleaned
+
