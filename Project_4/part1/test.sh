764ff77ebc418d43253b9ad4a44c55c7938a76f5
Jae-Won Jang <jjang3@vt.edu>
2018-11-07 21:35:00 -0500

    part1 finished

--- Project_4/part1/test.sh
+++ Project_4/part1/test.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
 sudo insmod proj4.ko
-cat /var/log/syslog | tail -2
-#cat /proc/proj4
+#cat /var/log/syslog | tail -2
+cat /proc/proj4
 rmmod proj4
 dmesg
-
-#dmesg -c > cleaned
+dmesg -c > cleaned
 
